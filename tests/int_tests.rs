34ff633875d2357eebc5725f5adc720faa2a1019
ryo-ebata <r.ebata@hajimari.inc>
2024-01-11 00:46:13 +0900

    fix: cargo test

--- tests/int_tests.rs
+++ tests/int_tests.rs
@@ -10,7 +10,7 @@ mod tests {
         assert_eq!(Int::new(-5000000000i64), Int::I64(-5000000000)); // Add type annotation
         assert_eq!(Int::new(200), Int::U8(200));
         assert_eq!(Int::new(60000), Int::U16(60000));
-        assert_eq!(Int::new(4000000000u64), Int::U64(4000000000)); // Add type annotation
-        assert_eq!(Int::new(10000000000000000000u128), Int::U128(10000000000000000000)); // Add type annotation
+        assert_eq!(Int::new(4000000000u64), Int::U32(4000000000)); // Add type annotation
+        assert_eq!(Int::new(10000000000000000000u128), Int::U64(10000000000000000000)); // Add type annotation
     }
 }
